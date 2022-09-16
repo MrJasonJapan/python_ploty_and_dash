016822ddde73b21d8db537ebda762b4f4fac4ae3
SpaGettys <jgettys@gmail.com>
2022-09-16 19:12:15 +0900

    Plot temperature data over seven days as a line scatterplot. (The data set is only for one location/place) ・Use list comprehension over each day of the week, and set x and y accordingly for each day. ・A for loop is another option and is commented out in the code.
    * There is no performance difference between using list comprehension versus for-loops. List comprehension makes the code shorter and cleaner, but for-loops have better readability.

--- 1-03E-LineChartExercises/Ex2-Linechart.py
+++ 1-03E-LineChartExercises/Ex2-Linechart.py
@@ -5,27 +5,35 @@
 ######
 
 # Perform imports here:
-
-
-
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
 
 # Create a pandas DataFrame from 2010YumaAZ.csv
 df = pd.read_csv('../data/2010YumaAZ.csv')
-days = ['TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY','MONDAY']
+days = ['TUESDAY', 'WEDNESDAY', 'THURSDAY',
+        'FRIDAY', 'SATURDAY', 'SUNDAY', 'MONDAY']
 
+# Use list comprehension to create traces for the data list.
+data = [go.Scatter(x=df['LST_TIME'], y=df[df['DAY'] == day]['T_HR_AVG'], mode='lines',
+                   name=day) for day in days]
 
-# Use a for loop (or list comprehension to create traces for the data list)
-data = []
+# pyo.plot(data)
 
-for day in days:
-    # What should go inside this Scatter call?
-    trace = go.Scatter()
-    data.append(trace)
+# Another way to prepare the data, but using a for loop instead.
+# data = []
+# for day in days:
+#     trace = go.Scatter(x=df[df['DAY'] == day]['LST_TIME'],y=df[df['DAY'] == day]['T_HR_AVG'], mode='lines', name = day)
+#     data.append(trace)
+
+# Yet another way to prepare the data, without the need for the days array at the top of this file.
+# data = [{'x': df['LST_TIME'], 'y': df[df['DAY'] == day]
+#          ['T_HR_AVG'], 'name': day} for day in df['DAY'].unique()]
 
 # Define the layout
-
-
-
-
+layout = go.Layout(title='Daily temp avgs')
 
 # Create a fig from data and layout, and plot the fig
+fig = go.Figure(data=data, layout=layout)
+
+pyo.plot(fig)
