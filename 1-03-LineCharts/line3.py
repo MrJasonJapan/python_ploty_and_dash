1ad69b0c9ecc99f026189b9fd2093749c907bbbb
SpaGettys <jgettys@gmail.com>
2022-09-16 18:33:55 +0900

    Use a real data set to create some more line graphs. ・ Filter on rows and columns to narrow-down the data set to something usable. ・ Take advantage of list-comprehension to populate our data list quickly and easily.

--- 1-03-LineCharts/line3.py
+++ 1-03-LineCharts/line3.py
@@ -9,25 +9,31 @@ import pandas as pd
 
 df = pd.read_csv('../sourcedata/nst-est2017-alldata.csv')
 # Alternatively:
-# df = pd.read_csv('https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/national/totals/nst-est2017-alldata.csv')
+#df = pd.read_csv('https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/national/totals/nst-est2017-alldata.csv')
+
+# print(df.head())
 
 # grab just the six New England states:
-df2 = df[df['DIVISION']=='1']
+df2 = df[df['DIVISION'] == '1']
+
 # set the index to state name:
 df2.set_index('NAME', inplace=True)
+
 # grab just the population columns:
 df2 = df2[[col for col in df2.columns if col.startswith('POP')]]
 
-traces=[go.Scatter(
-    x = df2.columns,
-    y = df2.loc[name],
-    mode = 'markers+lines',
-    name = name
-) for name in df2.index]
+# print(df2)
 
-layout = go.Layout(
-    title = 'Population Estimates of the Six New England States'
-)
+data = [go.Scatter(x=df2.columns, y=df2.loc[name],
+                     mode='lines',
+                     name=name
+                     ) for name in df2.index]
 
-fig = go.Figure(data=traces,layout=layout)
-pyo.plot(fig, filename='line3.html')
+pyo.plot(data)
+
+# layout = go.Layout(
+#     title = 'Population Estimates of the Six New England States'
+# )
+
+# fig = go.Figure(data=traces,layout=layout)
+# pyo.plot(fig, filename='line3.html')
