9272cbdc36d2ac9917c514ac9f7d3115e6f62a23
SpaGettys <jgettys@gmail.com>
2022-10-12 18:37:23 +0900

    Tinker with Bubble Charts (which are like Scatter Plots but with super-powered markers).

--- 1-05-BubbleCharts/bubble1.py
+++ 1-05-BubbleCharts/bubble1.py
@@ -7,21 +7,33 @@ import plotly.offline as pyo
 import plotly.graph_objs as go
 import pandas as pd
 
-df = pd.read_csv('../data/mpg.csv')
+df = pd.read_csv('../data/mpg.csv', na_values={'horsepower': '?'})
 
-data = [go.Scatter(          # start with a normal scatter plot
+# filter the dataframe where 'hoursepower' is numeric, and change the "horsepower" column to int64 type
+# df = df[df['horsepower'].apply(lambda x:x.isnumeric())]
+# df['horsepower'] = df['horsepower'].astype('int64')
+
+# start with a normal scatter plot
+data = [go.Scatter(
     x=df['horsepower'],
     y=df['mpg'],
-    text=df['name'],
+    text=df['name'],  # text displayed when hovering over a bubble
     mode='markers',
-    marker=dict(size=1.5*df['cylinders']) # set the marker size
+    # set the marker size. use a multiplier to make the bubbles more visible.
+    # marker=dict(size=1.5*df['cylinders'])
+    # set the marker size. divide by 100 to make the bubbles more visible. Add color based on the number of cylinders. Set a scale and set the name to "cylinders."
+    marker=dict(size=df['weight']/100, color=df['cylinders'],
+                showscale=True, colorbar={'title': 'Cylinders'})
 )]
 
+# setup the layout
 layout = go.Layout(
     title='Vehicle mpg vs. horsepower',
-    xaxis = dict(title = 'horsepower'), # x-axis label
-    yaxis = dict(title = 'mpg'),        # y-axis label
+    xaxis=dict(title='horsepower'),  # x-axis label
+    yaxis=dict(title='mpg'),        # y-axis label
     hovermode='closest'
 )
+
+# setup the figure and plot
 fig = go.Figure(data=data, layout=layout)
 pyo.plot(fig, filename='bubble1.html')
