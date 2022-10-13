790c77d8cb887f1120a8addd5269dbc9adee244a
SpaGettys <jgettys@gmail.com>
2022-10-13 15:01:04 +0900

    Tinkering with Box Plots

--- 1-06-BoxPlots/box1.py
+++ 1-06-BoxPlots/box1.py
@@ -6,14 +6,24 @@ import plotly.offline as pyo
 import plotly.graph_objs as go
 
 # set up an array of 20 data points, with 20 as the median value
-y = [1,14,14,15,16,18,18,19,19,20,20,23,24,26,27,27,28,29,33,54]
+y = [1, 14, 14, 15, 16, 18, 18, 19, 19, 20,
+     20, 23, 24, 26, 27, 27, 28, 29, 33, 54]
 
 data = [
+    # Good way to see all the data
     go.Box(
         y=y,
-        boxpoints='all', # display the original data points
+        boxpoints='all',  # display the original data points
         jitter=0.3,      # spread them out so they all appear
         pointpos=-1.8    # offset them to the left of the box
+    ),
+    # What you would normally do for box plot
+    go.Box(
+        y=y,
+        boxpoints='outliers',  # display the outlier data points only
+        jitter=0.3,      # spread them out so they all appear
+        pointpos=0    # offset them to the left of the box
     )
 ]
+
 pyo.plot(data, filename='box1.html')
--- 1-06-BoxPlots/box3.py
+++ 1-06-BoxPlots/box3.py
@@ -6,21 +6,21 @@
 import plotly.offline as pyo
 import plotly.graph_objs as go
 
-snodgrass = [.209,.205,.196,.210,.202,.207,.224,.223,.220,.201]
-twain = [.225,.262,.217,.240,.230,.229,.235,.217]
+snodgrass = [.209, .205, .196, .210, .202, .207, .224, .223, .220, .201]
+twain = [.225, .262, .217, .240, .230, .229, .235, .217]
 
 data = [
     go.Box(
         y=snodgrass,
-        name='QCS'
+        name='Snoggrass'
     ),
     go.Box(
         y=twain,
-        name='MT'
+        name='Twain'
     )
 ]
 layout = go.Layout(
-    title = 'Comparison of three-letter-word frequencies<br>\
+    title='Comparison of three-letter-word frequencies<br>\
     between Quintus Curtius Snodgrass and Mark Twain'
 )
 fig = go.Figure(data=data, layout=layout)
