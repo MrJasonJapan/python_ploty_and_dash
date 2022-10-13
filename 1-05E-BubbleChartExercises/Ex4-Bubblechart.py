5a95642d4cd8ffa0ee4e6d62421bb7f87dfa89a0
SpaGettys <jgettys@gmail.com>
2022-10-13 14:32:42 +0900

    Bubble Chart Excercise

--- 1-05E-BubbleChartExercises/Ex4-Bubblechart.py
+++ 1-05E-BubbleChartExercises/Ex4-Bubblechart.py
@@ -4,29 +4,34 @@
 # 'horsepower', 'weight', 'acceleration', 'model_year', 'origin', 'name'
 ######
 
-# Perform imports here:
+# Import ploty and pandas
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
 
+# Create a DataFrame from the .csv file, and replace horpower values of ? to null.
+df = pd.read_csv('../data/mpg.csv')
+print(df)
 
-
-
-# create a DataFrame from the .csv file:
-
-
-# create data by choosing fields for x, y and marker size attributes
-
-
-
-
-
-
-
+# create data with scatter plot, x=weight, y=mpg, text=name, with a mode of markers
+data = [go.Scatter(
+    x=df['weight'],
+    y=df['mpg'],
+    text=df['name'],
+    mode='markers',
+    # set the marker size based on accelleration, and show a color scale based on acceleration, with a title of acceleration
+    marker=dict(size=1.5*df['acceleration'], color=df['acceleration'],
+                showscale=True, colorbar={'title': 'Acceleration'})
+)]
 
 # create a layout with a title and axis labels
-
-
-
-
-
-
+layout = go.Layout(
+    title='Vehicle weight vs. Miles per gallon',
+    xaxis=dict(title='weight'),
+    yaxis=dict(title='mpg'),
+    hovermode='closest'
+)
 
 # create a fig from data & layout, and plot the fig
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='bubble_excercise.html')
