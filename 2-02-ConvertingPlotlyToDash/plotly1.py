328aa43dff87dfd5b015af383fe811d27be56b86
SpaGettys <jgettys@gmail.com>
2022-10-13 19:48:24 +0900

    Understanding and tinkering with the Dash app layout, but this time taking advantage of plotly as well

--- 2-02-ConvertingPlotlyToDash/plotly1.py
+++ 2-02-ConvertingPlotlyToDash/plotly1.py
@@ -7,8 +7,8 @@ import numpy as np
 app = dash.Dash()
 
 np.random.seed(42)
-random_x = np.random.randint(1,101,100)
-random_y = np.random.randint(1,101,100)
+random_x = np.random.randint(1, 101, 100)
+random_y = np.random.randint(1, 101, 100)
 
 app.layout = html.Div([
     dcc.Graph(
@@ -16,21 +16,21 @@ app.layout = html.Div([
         figure={
             'data': [
                 go.Scatter(
-                    x = random_x,
-                    y = random_y,
-                    mode = 'markers',
-                    marker = {
+                    x=random_x,
+                    y=random_y,
+                    mode='markers',
+                    marker={
                         'size': 12,
                         'color': 'rgb(51,204,153)',
                         'symbol': 'pentagon',
                         'line': {'width': 2}
-                        }
+                    }
                 )
             ],
             'layout': go.Layout(
-                title = 'Random Data Scatterplot',
-                xaxis = {'title': 'Some random x-values'},
-                yaxis = {'title': 'Some random y-values'},
+                title='Random Data Scatterplot',
+                xaxis={'title': 'Some random x-values'},
+                yaxis={'title': 'Some random y-values'},
                 hovermode='closest'
             )
         }
--- 2-03-SimpleDashboardExercise/Ex1-SimpleDashboard.py
+++ 2-03-SimpleDashboardExercise/Ex1-SimpleDashboard.py
@@ -7,39 +7,55 @@
 # 'Y' = waiting time until the next eruption in minutes (to nearest minute).
 ######
 
-# Perform imports here:
-
-
-
-
+# import dash related libraries, plotly, and pandas
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
 
 
 # Launch the application:
+app = dash.Dash()
 
 
 # Create a DataFrame from the .csv file:
+df = pd.read_csv('../data/OldFaithful.csv')
+# print the df shape, head, and columns
+print(df.shape)
+print(df.head())
+print(df.columns)
 
 
-# Create a Dash layout that contains a Graph component:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Create a Dash layout with a single scatterplot graph with 'X' and 'Y' on the axes:
+app.layout = html.Div([
+    dcc.Graph(
+        id='old_faithful',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df['X'],
+                    y=df['Y'],
+                    mode='markers',
+                    marker={
+                        'size': 12,
+                        'color': 'rgb(51,204,153)',
+                        'symbol': 'pentagon',
+                        'line': {'width': 2}
+                    }
+                )
+            ],
+            'layout': go.Layout(
+                title='Old Faithful Eruption Intervals v Durations',
+                xaxis={'title': 'Duration of eruption (minutes)'},
+                yaxis={'title': 'Interval to next eruption (minutes)'},
+                hovermode='closest'
+            )
+        }
+    )
+])
 
 
 # Add the server clause:
+if __name__ == '__main__':
+    app.run_server()
