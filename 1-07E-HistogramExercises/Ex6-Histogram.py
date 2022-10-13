c2541a58eae1b360d29154ccfa5a21313eb542f3
SpaGettys <jgettys@gmail.com>
2022-10-13 17:18:23 +0900

    Tinkering with Histograms (useful for continuous data)

--- 1-07E-HistogramExercises/Ex6-Histogram.py
+++ 1-07E-HistogramExercises/Ex6-Histogram.py
@@ -4,23 +4,31 @@
 # Set the range from 0 to 1, with a bin size of 0.02
 ######
 
-# Perform imports here:
-
-
+# import plotly and pandas
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
 
 
 # create a DataFrame from the .csv file:
+df = pd.read_csv('../data/abalone.csv')
+print(df)
 
 
-# create a data variable:
-
-
-
-
+# create a data variable with one Histogram using the 'length' field, with a range of 0 to 1, and a bin size of 0.02
+data = [
+    go.Histogram(
+        x=df['length'],
+        xbins=dict(start=0, end=1, size=0.02)
+    )
+]
 
 # add a layout
-
-
+layout = go.Layout(
+    title='Histogram of the length field from the Abalone dataset'
+)
 
 
 # create a fig from data & layout, and plot the fig
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='solution6.html')
