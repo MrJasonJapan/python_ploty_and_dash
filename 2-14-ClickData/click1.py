1be3690e160f3fe78e0219e907a799645a057c54
SpaGettys <jgettys@gmail.com>
2022-10-17 17:38:43 +0900

    Utilizing data that was clicked on in a visualization

--- 2-14-ClickData/click1.py
+++ 2-14-ClickData/click1.py
@@ -4,57 +4,64 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import pandas as pd
-import base64
+import base64  # need this for our encode_image function
 
 app = dash.Dash()
 
 df = pd.read_csv('../data/wheels.csv')
 
+
+# This function will encode an image into base64 format so the browser can display it.
 def encode_image(image_file):
     encoded = base64.b64encode(open(image_file, 'rb').read())
     return 'data:image/png;base64,{}'.format(encoded.decode())
 
+
 app.layout = html.Div([
     html.Div([
-    dcc.Graph(
-        id='wheels-plot',
-        figure={
-            'data': [
-                go.Scatter(
-                    x = df['color'],
-                    y = df['wheels'],
-                    dy = 1,
-                    mode = 'markers',
-                    marker = {
-                        'size': 12,
-                        'color': 'rgb(51,204,153)',
-                        'line': {'width': 2}
+        dcc.Graph(
+            id='wheels-plot',
+            figure={
+                'data': [
+                    go.Scatter(
+                        x=df['color'],
+                        y=df['wheels'],
+                        dy=1,
+                        mode='markers',
+                        marker={
+                            'size': 12,
+                            'color': 'rgb(51,204,153)',
+                            'line': {'width': 2}
                         }
+                    )
+                ],
+                'layout': go.Layout(
+                    title='Wheels & Colors Scatterplot',
+                    xaxis={'title': 'Color'},
+                    yaxis={'title': '# of Wheels', 'nticks': 3},
+                    hovermode='closest'
                 )
-            ],
-            'layout': go.Layout(
-                title = 'Wheels & Colors Scatterplot',
-                xaxis = {'title': 'Color'},
-                yaxis = {'title': '# of Wheels','nticks':3},
-                hovermode='closest'
-            )
-        }
-    )], style={'width':'30%', 'float':'left'}),
+            }
+        )], style={'width': '30%', 'float': 'left'}),
 
     html.Div([
-    html.Img(id='click-image', src='children', height=300)
-    ], style={'paddingTop':35})
+        # Image component is used to display the image corresponding to the clicked data point.
+        html.Img(id='click-image', src='', height=300)
+    ], style={'paddingTop': 35})
 ])
+
 
 @app.callback(
     Output('click-image', 'src'),
     [Input('wheels-plot', 'clickData')])
 def callback_image(clickData):
-    wheel=clickData['points'][0]['y']
-    color=clickData['points'][0]['x']
+    # grab x and y values from the clickData object and assign them to wheel and color respectively.
+    wheel = clickData['points'][0]['y']
+    color = clickData['points'][0]['x']
     path = '../data/images/'
-    return encode_image(path+df[(df['wheels']==wheel) & \
-    (df['color']==color)]['image'].values[0])
+    return encode_image(path+df[(df['wheels'] == wheel) &
+                                (df['color'] == color)]['image'].values[0])
+
 
 if __name__ == '__main__':
     app.run_server()
