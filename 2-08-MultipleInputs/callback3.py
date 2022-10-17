2dd811eeead22ca3fb2a896b1a834fce2e705b79
SpaGettys <jgettys@gmail.com>
2022-10-17 15:45:45 +0900

    Connecting multiple inputs to a single graph (figure) output

--- 2-08-MultipleInputs/callback3.py
+++ 2-08-MultipleInputs/callback3.py
@@ -12,35 +12,40 @@ import pandas as pd
 app = dash.Dash()
 
 df = pd.read_csv('../data/mpg.csv')
+# print the df's shape, size, columns in one line, with labels.
+print('df.shape: ', df.shape, 'df.size: ', df.size, 'df.columns: ', df.columns, sep='\n')
+print(df.head())
 
+
+# list of columms in df: ['mpg', 'cylinders', 'displacement', 'horsepower', 'weight', 'acceleration', 'model_year', 'origin', 'name']
 features = df.columns
 
 app.layout = html.Div([
 
-        html.Div([
-            dcc.Dropdown(
-                id='xaxis',
-                options=[{'label': i.title(), 'value': i} for i in features],
-                value='displacement'
-            )
-        ],
-        style={'width': '48%', 'display': 'inline-block'}),
+    html.Div([
+        dcc.Dropdown(
+            id='xaxis',
+            # title() means the first letter of each word is capitalized.
+            options=[{'label': i.title(), 'value': i} for i in features],
+            value='displacement'
+        )
+    ], style={'width': '48%', 'display': 'inline-block'}),
 
-        html.Div([
-            dcc.Dropdown(
-                id='yaxis',
-                options=[{'label': i.title(), 'value': i} for i in features],
-                value='acceleration'
-            )
-        ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'}),
+    html.Div([
+        dcc.Dropdown(
+            id='yaxis',
+            options=[{'label': i.title(), 'value': i} for i in features],
+            value='acceleration'
+        )
+    ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'}),
 
     dcc.Graph(id='feature-graphic')
-], style={'padding':10})
+], style={'padding': 10})
+
 
 @app.callback(
     Output('feature-graphic', 'figure'),
-    [Input('xaxis', 'value'),
-     Input('yaxis', 'value')])
+    [Input('xaxis', 'value'), Input('yaxis', 'value')])
 def update_graph(xaxis_name, yaxis_name):
     return {
         'data': [go.Scatter(
@@ -51,7 +56,7 @@ def update_graph(xaxis_name, yaxis_name):
             marker={
                 'size': 15,
                 'opacity': 0.5,
-                'line': {'width': 0.5, 'color': 'white'}
+                'line': {'width': 0.5, 'color': 'white'}  # border line around each marker
             }
         )],
         'layout': go.Layout(
@@ -61,6 +66,7 @@ def update_graph(xaxis_name, yaxis_name):
             hovermode='closest'
         )
     }
+
 
 if __name__ == '__main__':
     app.run_server()
